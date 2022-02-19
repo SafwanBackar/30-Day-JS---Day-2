ed7793571454101afa62fbef23c3e8fa7516fc99
SafwanBackar <sbackar29@gmail.com>
2022-02-19 15:33:28 +0530

    day 2 work

---
+++ main.js
@@ -0,0 +1,98 @@
+var challenge = "30 Days of JavaScript";
+
+
+
+
+console.log(challenge)
+
+
+console.log(challenge.lengh)
+
+
+challenge.length
+
+console.log(challenge.length)
+
+
+console.log(challenge.toUpperCase())
+
+console.log(challenge.toLowerCase())
+
+console.log(challenge.substr(0, 2))
+
+console.log(challenge.includes("Script"))
+
+
+console.log(challenge.split())
+
+
+console.log(challenge.split(" "))
+
+var faang = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+console.log(faang.split(","))
+
+console.log(challenge.replace("JavaScript", "Python"))
+
+console.log(challenge.charAt(15))
+
+console.log(challenge.charAt("J"))
+
+
+console.log(challenge.indexOf("a"))
+
+console.log(challenge.lastIndexOf("a"))
+
+var sentence = "You cannot end a sentence with because because because is a conjunction"
+
+console.log(sentence.indexOf("because"))
+
+
+console.log(sentence.lastIndexOf("because"))
+
+
+console.log(sentence.search("because"))
+
+
+console.log(challenge.startsWith("30"))
+console.log(challenge.endsWith("t"))
+
+
+
+let pattern = /a/gi
+console.log(challenge.match(pattern))
+
+console.log(challenge.repeat(2))
+
+
+let chat = "30";
+console.log(chat.concat(" Days", " of", " JavaScript Yo"))
+
+// Exercise 2
+
+console.log("The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.")
+console.log("Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.")
+
+var next = parseFloat('9.8')
+
+var next2 = Math.ceil(next)
+
+console.log(next2)
+
+
+// var py = python;
+// var jar = jargon;
+// console.log(
+// )
+
+
+// 10.
+
+var hat = "JavaScript";
+console.log(hat.indexOf("a"))
+
+
+console.log("1 1 1 1 1")
+
+
+
